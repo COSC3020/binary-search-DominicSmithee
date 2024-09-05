422a2fdc5b09ead0e87ed20cafc918a41242efc3
Dominic Smith <60902371+DominicSmithee@users.noreply.github.com>
2024-09-05 14:09:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,16 +1,36 @@
-function binarySearch(list, element) {
-    let position = 0;
-    if(list.length > 0){  
-        for(i=0;i<list.length-1;i++){
-            if(list[i] == element){
-                position = i;
-            }
-        }
-        return position;
+function binarySearch(list, element){
+    if(list.length == 1){
+        var mid = list[0];
     }else{
-        return -1;
+        var mid = list[list.length/2];
     }
+    console.log(mid);
+    console.log(list);
+    if(list.length > 0){
+        if(mid == element && list.length == 2){
+                
+                console.log('case 1');
+                //console.log(list);
+                //console.log(binarySearch.count);
+                return list.indexOf(mid);
+              
+        }else if(element < mid){
+                console.log('case 2');
+                return binarySearch(list.slice(0,list.length/2), element);
+        }else if(element > mid){
+                console.log('case 3');
+                return binarySearch(list.slice(list.length/2, list.length), element);
+        }else{
+            return;
+        }
+        //console.log(list);
+        return list.indexOf(mid);
+        }else{
+            console.log('case 4');
+            return -1;
+        }
 }
 
-//listy = [0,1,2,3,4,5,6,7];
-//console.log(binarySearch(listy,3));
+listy = [7,8,9,10];
+elementy = 10;
+console.log(binarySearch(listy, elementy));
