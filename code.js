2f9f5b8c13aaeb70691e736444836df820a605ef
Dominic Smith <60902371+DominicSmithee@users.noreply.github.com>
2024-09-05 10:27:35 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function binarySearch(list, element) {
-    return -1;
+    let position = 0;
+    if(list.length > 0){  
+        for(i=0;i<list.length;i++){
+            if(list[i] == element){
+                position = i;
+            }
+        }
+        return position;
+    }else{
+        return -1;
+    }
 }
+
+//listy = [0,1,2,3,4,5,6,7];
+//console.log(binarySearch(listy,3));
